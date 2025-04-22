07c33a96bdeec0037e90d9fe92862014900377aa
Luizhlf <luiz301202lh@gmail.com>
2025-04-22 11:42:44 -0300

    exercicio 10

--- ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria.cpp
@@ -152,5 +152,17 @@ int elementosArvore(NO* no)
 
 void exibirElementosArvore(NO* no)
 {
-	
+	if (no == NULL) {
+		return;
+	}
+	else {
+
+		exibirElementosArvore(no->esq);
+
+		cout << " " << no->valor << endl;
+
+		exibirElementosArvore(no->dir);
+	}
+
+
 }
